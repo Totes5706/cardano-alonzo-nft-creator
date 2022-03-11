4fae4ed01cf2bd9c82cef0d77ebe81ec08a05c9f
Joe Totes <totinj@gmail.com>
2022-03-11 10:45:07 -0500

    generate key pairs and address

--- make_nft.bash
+++ make_nft.bash
@@ -9,7 +9,7 @@ export CARDANO_NODE_SOCKET_PATH=$nodepath
 #specify from the user whether they are using either the testnet or mainnet and declare 
 echo Which Cardano network will you be using?
 
-select MAGIC in 'mainnet' 'testnet' 
+select NETWORK in 'mainnet' 'testnet' 
 do
     break
 done
@@ -17,16 +17,16 @@ done
 case $NETWORK in
 
     mainnet) 
-        MAGIC='mainnet'
+        NETWORK='--mainnet'
         ;;
     
     testnet)
-        MAGIC='testnet-magic 1097911063' 
+        NETWORK='--testnet-magic 1097911063' 
         ;;
 esac
 
 #get the NFT name and IPFS CID location from the user
-read -p 'Enter of the NFT want to create (no spaces) : ' nftname
+read -p 'Enter of the NFT name you want to create (no spaces) : ' nftname
 read -p 'Enter the unique IPFS CID associated with the NFT : ' ipfs_cid
 
 #change to NFT directory and create new folder with the NFT name
@@ -35,3 +35,54 @@ mkdir -p $nftname
 
 #change to newly created token name folder
 cd $nftname
+
+#generate vrf keys .vkey and .skey for the transaction, asking user permission to replace if files already exist
+if [ -f payment.skey || -f payment.vkey  ]; then
+    echo "Vrf key files already exist. Would you like to overwrite?"
+
+    select overwrite in 'yes' 'no' 
+    do
+        break
+    done
+
+    case $overwrite in
+
+    yes) 
+        cardano-cli address key-gen --verification-key-file payment.vkey --signing-key-file payment.skey 
+        ;;
+    
+    no)
+        echo "Original vrf files left unchanged"
+        ;;
+    esac
+
+else 
+    cardano-cli address key-gen --verification-key-file payment.vkey --signing-key-file payment.skey 
+
+fi
+
+
+#generate payment recieve address for the transaction
+if [ -f payment.addr ]; then
+    echo "Payment address already exists. Would you like to overwrite?"
+
+    select overwrite in 'yes' 'no' 
+    do
+        break
+    done
+
+    case $overwrite in
+
+    yes) 
+        cardano-cli address build --payment-verification-key-file payment.vkey --out-file payment.addr $NETWORK)
+        ;;
+    
+    no)
+        echo "original address file left unchanged"
+        ;;
+    esac
+
+else 
+    cardano-cli address build --payment-verification-key-file payment.vkey --out-file payment.addr $NETWORK)
+
+fi
