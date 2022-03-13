e6662952fb46460b980e885b0db7fa234f5c137a
Joe Totes <totinj@gmail.com>
2022-03-13 19:57:50 -0400

    Removed un-used imports

--- src/Utils.hs
+++ src/Utils.hs
@@ -8,31 +8,16 @@ module Utils
     ) where
 
 import           Cardano.Api                 as API
-import           Cardano.Api.Shelley         (Address (..), PlutusScript (..))
-import           Cardano.Crypto.Hash.Class   (hashToBytes)
-import           Cardano.Ledger.Credential   as Ledger
-import           Cardano.Ledger.Crypto       (StandardCrypto)
-import           Cardano.Ledger.Hashes       (ScriptHash (..))
-import           Cardano.Ledger.Keys         (KeyHash (..))
+import           Cardano.Api.Shelley         (PlutusScript (..))
 import           Codec.Serialise             (serialise)
-import           Data.Aeson                  (decode, encode)
 import qualified Data.ByteString.Char8       as BS8
 import qualified Data.ByteString.Lazy        as LBS
 import qualified Data.ByteString.Short       as SBS
-import           Data.Maybe                  (fromJust, fromMaybe)
+import           Data.Maybe                  (fromJust)
 import           Data.String                 (IsString (..))
-import           Data.Text                   (pack)
-import           Plutus.PAB.Webserver.Types  (ContractActivationArgs (..))
-import           Plutus.V1.Ledger.Credential as Plutus
-import           Plutus.V1.Ledger.Crypto     as Plutus
-import           Plutus.V1.Ledger.Value      (TokenName (..))
-import           PlutusTx                    (Data (..))
-import qualified PlutusTx
-import           PlutusTx.Builtins           (toBuiltin)
+import           Plutus.V1.Ledger.Value      (TokenName (..)) 
 import           PlutusTx.Builtins.Internal  (BuiltinByteString (..))
 import qualified Ledger                      as Plutus
-import           Wallet.Emulator.Wallet      (WalletId (..), Wallet (..))
-import           Wallet.Types                (ContractInstanceId (..))
 
 
 unsafeReadTxOutRef :: String -> Plutus.TxOutRef
