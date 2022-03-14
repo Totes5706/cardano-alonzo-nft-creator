0ee642a98a84c8c3af2e20e7826377b1cef3468e
Joe Totes <59018247+Totes5706@users.noreply.github.com>
2022-03-13 21:26:42 -0400

    Update start-testnet-node.sh

--- node/start-testnet-node.sh
+++ node/start-testnet-node.sh
@@ -4,5 +4,5 @@ cardano-node run \
     --config testnet/testnet-config.json \
     --topology testnet/testnet-topology.json \
     --database-path testnet/db \
-    --socket-path testnet/node.sock \
+    --socket-path testnet/node0.socket \
     --port 3003
