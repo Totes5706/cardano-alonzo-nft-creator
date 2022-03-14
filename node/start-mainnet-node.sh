d75fdddd2777158e22191f870d41bbeede4ef4df
Joe Totes <59018247+Totes5706@users.noreply.github.com>
2022-03-13 21:26:21 -0400

    Update start-mainnet-node.sh

--- node/start-mainnet-node.sh
+++ node/start-mainnet-node.sh
@@ -4,5 +4,5 @@ cardano-node run \
     --config mainnet/mainnet-config.json \
     --topology mainnet/mainnet-topology.json \
     --database-path mainnet/db \
-    --socket-path mainnet/node.sock \
+    --socket-path mainnet/node0.socket \
     --port 3003
